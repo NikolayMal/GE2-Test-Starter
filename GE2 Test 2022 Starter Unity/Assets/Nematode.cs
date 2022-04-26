29430e19a7c8fca3a2e010a254b3c36160d2d07e
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2022-04-26 10:14:14 +0100

    Nematode Movement
    It now moves up down, left right using the NoiseWander scripts

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -6,28 +6,29 @@ public class Nematode : MonoBehaviour
 {
     public int length;
 
-    public Material material;
+    public Material material;    
 
     void Awake()
     {
         // Put your code here!
-        
-        
         for (int i = 0; i < length; i++) {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.position = transform.position - transform.forward * i;
             sphere.transform.parent = transform;
             sphere.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)length, 1, 1);
-            sphere.transform.localScale = Vector3.one * (i + 1) / length;
+            // Attach noise wanderer script to first sphere
+            if (i == 0) {
+                sphere.AddComponent<NoiseWander>();
+                sphere.AddComponent<NoiseWanderV>();
+                sphere.AddComponent<ObstacleAvoidance>();
+            }
             
-            // if (i <= length / 2) {
-            //     sphere.transform.localScale = Vector3.one * (i + 1) / length;
-            // }
-            // if (i > length / 2) {
-            //     sphere.transform.localScale = Vector3.one * (i - 1) / length;                
-            // }           
-            
-
+            if (i <= length / 2) {
+                sphere.transform.localScale = Vector3.one * (i + 1) / length;
+            }
+            if (i > length / 2) {
+                sphere.transform.localScale = Vector3.one * (i - 1) / length;                
+            }           
         }
     }
 
@@ -42,6 +43,7 @@ public class Nematode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
         
     }
 }
