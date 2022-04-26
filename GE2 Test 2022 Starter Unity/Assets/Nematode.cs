49cbaee53f232161af6500ea6f2d5d00928f5cdf
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2022-04-26 09:44:29 +0100

    Nematode Segments
    Segments created, color applied, size increases, but does not decrease after halfway

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class Nematode : MonoBehaviour
 {
-    public int length = 5;
+    public int length;
 
     public Material material;
 
     void Awake()
     {
         // Put your code here!
+        
+        
+        for (int i = 0; i < length; i++) {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = transform.position - transform.forward * i;
+            sphere.transform.parent = transform;
+            sphere.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)length, 1, 1);
+            sphere.transform.localScale = Vector3.one * (i + 1) / length;
+            
+            // if (i <= length / 2) {
+            //     sphere.transform.localScale = Vector3.one * (i + 1) / length;
+            // }
+            // if (i > length / 2) {
+            //     sphere.transform.localScale = Vector3.one * (i - 1) / length;                
+            // }           
+            
+
+        }
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+
         
     }
 
